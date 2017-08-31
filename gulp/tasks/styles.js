96766862ee9a4d8890edab04d2134d6edfb1a178
b-phew <barry@phewprojects.com>
2017-08-31 19:10:48 +0100

    Styling the header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,10 +5,11 @@ var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var importcss = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([importcss, mixins, cssvars, nested, autoprefixer]))
+	.pipe(postcss([importcss, mixins, cssvars, nested, hexrgba, autoprefixer]))
 	.on('error',  function(errorInfo) {
 		console.log(errorInfo.toString());
 		this.emit('end');
