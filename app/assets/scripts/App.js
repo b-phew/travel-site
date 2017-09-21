7a7626d972a63f7fa23deda78eb2ff8644020f41
b-phew <barry@phewprojects.com>
2017-09-21 10:33:20 +0100

    Finnished added the reveal on scroll to the featured item section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/reveal-on-scroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
